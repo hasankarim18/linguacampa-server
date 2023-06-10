152b62048e56b1f9cdd7d5cc78ba45c16c6970d4
hasankarim18 <hallysust@gmail.com>
2023-06-10 23:55:41 +0600

    admin role change api implemented

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000
 
+
+
 /**
  * Mongodb
  */
@@ -28,8 +30,29 @@ const port = process.env.PORT || 5000
 const userName = process.env.USER_NAME;
 const password = process.env.PASSWORD
 
+const secret = process.env.secret_key;
 
+/** verify jwt */
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if(!authorization){
+    return res.status(401).send({error:true, message:"unathorized access"})
+  }
+
+  const token = authorization.split(' ')[1]
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri =
   `mongodb+srv://${userName}:${password}@cluster0.nzfxe6e.mongodb.net/?retryWrites=true&w=majority`;
@@ -51,6 +74,104 @@ async function run() {
     const database = client.db("linguaCampa");
     const instructorCollection = database.collection("instructors");
     const usersCollections = database.collection("users");
+
+    /**
+     * ***********************************************************
+     *       sending token to the front end
+     *  **********************************************************
+     *  */
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, secret, { expiresIn: "4h" });
+
+      res.send({ token });
+    });
+    /**
+     * ***********************************************************
+     *     WARNING use veriryJWT before useing verifyAdmin
+     * **********************************************************
+     *  */
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await usersCollections.findOne(query);
+
+        if (user?.role !== "admin") {
+          return res
+            .status(403)
+            .send({ error: true, message: "forbidden message", data: [] });
+        }
+        next();
+      };
+
+    /**
+     * ***********************************************************
+     *   *  WHEN A USER LOGGED IN IT VERIFIES ITS ROLE AND GIVE ROLE RETURN
+     * **********************************************************
+     *  */
+    app.get("/users/role/:email", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+
+      const email = req.params.email;
+     
+
+      if (decodedEmail !== email) {
+        return res.status(401).send({ admin: false });
+      }
+
+      const query = { email: email };
+
+      const user = await usersCollections.findOne(query);
+      const result = user?.role 
+     // const result = { role: user?.role };
+      res.send(result);
+    });
+
+    /**
+     * Admin Only Get all user data 
+     */
+    
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res)=> {
+      try {
+        const cursor = usersCollections.find()
+        const result = await cursor.toArray()
+        res.send({message:'success', data:result})
+      } catch (error) {
+         res.send({ message: "error", data: error });
+      }
+    } )
+
+    /**
+     * Admin only update role 
+     */
+
+    app.patch('/users', verifyJWT, verifyAdmin ,async (req, res)=> {
+      try {
+         const query = req.query;
+       //  console.log(query);
+         const email = query.email
+         const role = query.role
+
+        const filter = { email: email };
+        
+        const options = { upsert: true };
+       // create a document that sets the plot of the movie
+        const updateDoc = {
+          $set: {
+            role: role,
+            applied: false,
+          }         
+        };
+        const result = await usersCollections.updateOne(filter, updateDoc, options);
+
+
+         res.send({ message: "success", data: result });
+      } catch (error) {
+         res.send({ message: "error" });
+      }
+     
+    } )
+
 
     app.get("/classes", async (req, res) => {
       try {
@@ -78,18 +199,18 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       user.role = "student";
-     const userEmail = user.email;
-    
-     const query = { email: userEmail };
+      const userEmail = user.email;
 
-     const existingUser = await usersCollections.findOne(query);
+      const query = { email: userEmail };
 
-     if (existingUser) { 
-       return res.send({ message: "user already exists" });
-     } else {
-       const result = await usersCollections.insertOne(user);
-       res.send(result);
-     }
+      const existingUser = await usersCollections.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      } else {
+        const result = await usersCollections.insertOne(user);
+        res.send(result);
+      }
     });
 
     // Send a ping to confirm a successful connection
