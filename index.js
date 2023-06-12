0e5796899c27f6342153f3c65857183ab43dca23
hasankarim18 <hallysust@gmail.com>
2023-06-12 16:49:24 +0600

    update

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ async function run() {
     const feedbackCollections = database.collection("feedback");
     const selectedCollections = database.collection("selected");
     const paymentsCollections = database.collection("payments");
-    const enrollCollections = database.collection('enrolled')
+    const enrollCollections = database.collection("enrolled");
 
     /**
      * ***********************************************************
@@ -158,31 +158,29 @@ async function run() {
     /***Student related apis */
     app.post("/selectClass", verifyJWT, verifyStudent, async (req, res) => {
       try {
-
         const body = req.body;
         const classId = body.classId;
         const studentEmail = body.studenEmail;
 
         const isExistsQuery = {
           studentEmail: studentEmail,
-          classId:classId
+          classId: classId,
         };
 
         const isEnrolled = await enrollCollections.findOne({
           studentId: studentEmail,
           enrolledClassIds: { $in: [classId] },
         });
-        const isExists = await selectedCollections.findOne(isExistsQuery)
+        const isExists = await selectedCollections.findOne(isExistsQuery);
 
-        console.log(isEnrolled)
-       
+        console.log(isEnrolled);
 
-        if(isExists){
-          return res.send({message:"already_selected"})
-        }else {
-          if(isEnrolled){
-             return res.send({ message: "already_enrolled" });
-          }else {
+        if (isExists) {
+          return res.send({ message: "already_selected" });
+        } else {
+          if (isEnrolled) {
+            return res.send({ message: "already_enrolled" });
+          } else {
             const date = new Date();
             const doc = {
               classId,
@@ -191,8 +189,8 @@ async function run() {
             };
             const result = await selectedCollections.insertOne(doc);
             res.send({ message: "success" });
-          }           
-        }       
+          }
+        }
       } catch (error) {
         res.send({ message: "error" });
       }
@@ -242,7 +240,7 @@ async function run() {
 
           const result = await cursor.toArray();
 
-    //    const obId = result.map(item => item._id)
+          //    const obId = result.map(item => item._id)
 
           const classIds = result.map((item) => item.classId);
 
@@ -251,10 +249,10 @@ async function run() {
           };
 
           const result2 = await classesCollections.find(query2).toArray();
-         // result2.selectedItemObjectId = obId;
-        //  console.log('[result2]',result2) 
+          // result2.selectedItemObjectId = obId;
+          //  console.log('[result2]',result2)
 
-          res.send({ message: "success", data: result2  });
+          res.send({ message: "success", data: result2 });
         } catch (error) {
           res.send({ message: "error" });
         }
@@ -475,23 +473,6 @@ async function run() {
             };
           });
 
-          /**************
-           * 
-           * instructor home 
-           */
-
-          app.get(
-            "/instructorHome/:email",
-            verifyJWT,
-            verifyInstructor,
-            async (req, res) => {
-              const email = req.params.email;
-            
-              const result = await usersCollections.findOne({ email: email });
-              res.send(result);
-            }
-          );
-
           // console.log('conbine ',combinedResults);
           res.send({ message: "success", data: combinedResults });
         } catch (error) {
@@ -504,6 +485,24 @@ async function run() {
     /**
      * instructor only view class only own his class
      */
+
+    /**************
+     *
+     * instructor home
+     */
+
+    app.get(
+      "/instructorHome/:email",
+      verifyJWT,
+      verifyInstructor,
+      async (req, res) => {
+        const email = req.params.email;
+        console.log(email);
+
+        const result = await usersCollections.findOne({ email: email });
+        res.send(result);
+      }
+    );
 
     app.get("/classes/:email", async (req, res) => {
       try {
@@ -549,7 +548,7 @@ async function run() {
 
     app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;    
+      const amount = price * 100;
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
@@ -563,126 +562,130 @@ async function run() {
     });
 
     /**#paymentsCollections #paymentHistory */
-    app.post('/payments',verifyJWT,verifyStudent,  async (req, res)=> {
-      const payment = req.body     
-      const insertResult = await paymentsCollections.insertOne(payment)
-
+    app.post("/payments", verifyJWT, verifyStudent, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentsCollections.insertOne(payment);
 
       const query = {
-        studentEmail: payment.user
+        studentEmail: payment.user,
       };
 
-        const deleteResult = await selectedCollections.deleteMany(query)
+      const deleteResult = await selectedCollections.deleteMany(query);
 
-       const existingStudent = await enrollCollections.findOne({studentId:payment.user});    
-         
-       if(existingStudent){
-      //  console.log('user Exists',existingStudent);
-          const updatedEnrolledClassIds = [
-            ...existingStudent.enrolledClassIds,
-            ...payment.enrolledClassIds,
-          ];
+      const existingStudent = await enrollCollections.findOne({
+        studentId: payment.user,
+      });
 
-          const updateResult = await enrollCollections.updateOne(
-            { studentId: payment.user },
-            { $set: { enrolledClassIds: updatedEnrolledClassIds } }
-          );
+      if (existingStudent) {
+        //  console.log('user Exists',existingStudent);
+        const updatedEnrolledClassIds = [
+          ...existingStudent.enrolledClassIds,
+          ...payment.enrolledClassIds,
+        ];
 
-          if (updateResult.modifiedCount > 0) {
-            // The enrolledClassIds array has been updated successfully
-            res.send({ insertResult, deleteResult, updateResult });
-          } else {
-            // Failed to update the enrolledClassIds array
-            res.status(500).send("Failed to update enrolledClassIds array.");
-          }
+        const updateResult = await enrollCollections.updateOne(
+          { studentId: payment.user },
+          { $set: { enrolledClassIds: updatedEnrolledClassIds } }
+        );
 
-       }else {
-           const enrolled = payment.enrolledClassIds;
-           const studentEmail = payment.user;
-          const enrolldoc = {
-             enrolledClassIds: enrolled,
-             studentId: studentEmail,
-             date: new Date(),
-           };      
-         // console.log('user not exist');
+        if (updateResult.modifiedCount > 0) {
+          // The enrolledClassIds array has been updated successfully
+          res.send({ insertResult, deleteResult, updateResult });
+        } else {
+          // Failed to update the enrolledClassIds array
+          res.status(500).send("Failed to update enrolledClassIds array.");
+        }
+      } else {
+        const enrolled = payment.enrolledClassIds;
+        const studentEmail = payment.user;
+        const enrolldoc = {
+          enrolledClassIds: enrolled,
+          studentId: studentEmail,
+          date: new Date(),
+        };
+        // console.log('user not exist');
 
-           const enrolledResult = await enrollCollections.insertOne(enrolldoc);
-           res.send({ insertResult, deleteResult, enrolledResult });
-       }     
-   
-    } )
+        const enrolledResult = await enrollCollections.insertOne(enrolldoc);
+        res.send({ insertResult, deleteResult, enrolledResult });
+      }
+    });
 
     /** student payment history */
 
-    app.get('/paymentHistory/:email', verifyJWT, verifyStudent, async (req, res)=> {
-      const email = req.params.email
+    app.get(
+      "/paymentHistory/:email",
+      verifyJWT,
+      verifyStudent,
+      async (req, res) => {
+        const email = req.params.email;
 
-      const payments = await paymentsCollections
-        .find({user:email})
-        .sort({ date: -1 })
-        .toArray();
+        const payments = await paymentsCollections
+          .find({ user: email })
+          .sort({ date: -1 })
+          .toArray();
 
-        res.send({message:"success",data:payments})
-
-    } )
+        res.send({ message: "success", data: payments });
+      }
+    );
 
     /** enrolled classes */
 
-    app.get('/enrolledClasses/:email', verifyJWT, verifyStudent, async (req, res)=> {
-      try {
-        const email = req.params.email 
-        const isEnrolled = await enrollCollections.findOne({ studentId :email});
+    app.get(
+      "/enrolledClasses/:email",
+      verifyJWT,
+      verifyStudent,
+      async (req, res) => {
+        try {
+          const email = req.params.email;
+          const isEnrolled = await enrollCollections.findOne({
+            studentId: email,
+          });
 
-        if(isEnrolled){
-          const eid = isEnrolled.enrolledClassIds
+          if (isEnrolled) {
+            const eid = isEnrolled.enrolledClassIds;
 
-          const enrolledClasses = await classesCollections
-            .find({ _id: { $in: eid.map(item => new ObjectId(item)) } })
-            .toArray();
+            const enrolledClasses = await classesCollections
+              .find({ _id: { $in: eid.map((item) => new ObjectId(item)) } })
+              .toArray();
 
             res.send({ message: "success", data: enrolledClasses });
-        }else {
-           res.send({ message: "not_enrolled", data: []});
-        }
-
-      
-      } catch (error) {
-        res.send({message:"errror"})
-      }
-    })
-
-      app.get("/updateSeats", async (req, res) => {
-        try {
-          const enrolledStudents = await enrollCollections.find().toArray();
-          const classes = await classesCollections
-            .find({ status: "approved" })
-            .toArray();
-
-          for (const classObj of classes) {
-            const classId = classObj._id;
-            const enrolledCount = enrolledStudents.filter((student) =>
-              student.enrolledClassIds.includes(classId)
-            ).length;
-            const seatsLeft = classObj.seats - enrolledCount;
-
-            console.log(enrolledCount);
-
-            // await classesCollections.updateOne(
-            //   { _id: classId },
-            //   { $set: { enrolledStudents: enrolledCount, seatsLeft: seatsLeft } }
-            // );
+          } else {
+            res.send({ message: "not_enrolled", data: [] });
           }
-
-          res.send("Seats updated successfully");
         } catch (error) {
-          console.error(error);
-          res.status(500).send("Error updating seats");
+          res.send({ message: "errror" });
         }
-      });
+      }
+    );
 
+    app.get("/updateSeats", async (req, res) => {
+      try {
+        const enrolledStudents = await enrollCollections.find().toArray();
+        const classes = await classesCollections
+          .find({ status: "approved" })
+          .toArray();
 
+        for (const classObj of classes) {
+          const classId = classObj._id;
+          const enrolledCount = enrolledStudents.filter((student) =>
+            student.enrolledClassIds.includes(classId)
+          ).length;
+          const seatsLeft = classObj.seats - enrolledCount;
 
+          console.log(enrolledCount);
 
+          // await classesCollections.updateOne(
+          //   { _id: classId },
+          //   { $set: { enrolledStudents: enrolledCount, seatsLeft: seatsLeft } }
+          // );
+        }
+
+        res.send("Seats updated successfully");
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Error updating seats");
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
